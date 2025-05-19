40d431fb98a457332172bcb189d176d179153e4d
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2025-05-19 15:45:53 +0200

    change init to lazy loader

--- src/harpy/__init__.py
+++ src/harpy/__init__.py
@@ -1,24 +1,39 @@
 """Define package version"""
+
 import importlib.metadata
-
-__version__ = importlib.metadata.version("harpy-analysis")
-
 import os
 
+__version__ = importlib.metadata.version("harpy-analysis")
 os.environ["USE_PYGEOS"] = "0"
 
-try:
-    import rasterio
-except ImportError:
-    pass
+# silence dask future warnings
+import warnings
 
-from harpy import (  # noqa: E402
-    datasets,
-    io,
-    utils,
-)
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+import dask
+
+dask.config.set({'dataframe.query-planning': False})
+
+
+from harpy import datasets
 from harpy import image as im  # noqa: E402
+from harpy import io
 from harpy import plot as pl  # noqa: E402
 from harpy import points as pt  # noqa: E402
 from harpy import shape as sh  # noqa: E402
 from harpy import table as tb  # noqa: E402
+from harpy import utils
+
+__all__ = [
+    "datasets",
+    "io",
+    "utils",
+    "im",
+    "pl",
+    "pt",
+    "sh",
+    "tb",
+    "__version__",
+]
--- src/harpy/shape/__init__.py
+++ src/harpy/shape/__init__.py
@@ -1,2 +1,14 @@
-from ._cell_expansion import create_voronoi_boundaries
-from ._shape import add_shapes_layer, filter_shapes_layer, intersect_rectangles, vectorize
+# from ._cell_expansion import create_voronoi_boundaries
+# from ._shape import add_shapes_layer, filter_shapes_layer, intersect_rectangles, vectorize
+
+import lazy_loader as lazy
+
+__getattr__, __dir__, __all__ = lazy.attach_stub(__name__, __file__)
+
+# __getattr__, __dir__, __all__ = lazy.attach(
+#     __name__,
+#     submod_attrs={
+#         '_cell_expansion': ['create_voronoi_boundaries'],
+#         '_shape': ['add_shapes_layer', 'filter_shapes_layer', 'intersect_rectangles', 'vectorize'],
+#     }
+# )
