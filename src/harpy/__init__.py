d594cd8f8c7b85d659dd4bcb2ee8251fc3890861
Benjamin Rombaut <benjamin.rombaut@gmail.com>
2025-05-20 15:53:14 +0200

    filter future and deprecation warnings

--- src/harpy/__init__.py
+++ src/harpy/__init__.py
@@ -4,27 +4,31 @@ import importlib.metadata
 import os
 
 __version__ = importlib.metadata.version("harpy-analysis")
+
+# see geopandas https://geopandas.org/en/stable/ and https://github.com/geopandas/geopandas/releases/tag/v1.0.0
+# removing this could mean only supporting gepandas >=1.0.0 and shapely 2
 os.environ["USE_PYGEOS"] = "0"
 
-# silence dask future warnings
-import warnings
+loglevel = os.environ.get("LOGLEVEL")
+if isinstance(loglevel, str) and loglevel.upper() != "DEBUG":
+    # silence developer warnings if not in debug mode
+    import warnings
 
-warnings.filterwarnings("ignore", category=FutureWarning)
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+# avoid newer dataframe backends, see https://github.com/dask/dask/issues/11146
 import dask
 
-dask.config.set({'dataframe.query-planning': False})
+dask.config.set({"dataframe.query-planning": False})
 
 
-from harpy import datasets
+from harpy import datasets, io, utils
 from harpy import image as im  # noqa: E402
-from harpy import io
 from harpy import plot as pl  # noqa: E402
 from harpy import points as pt  # noqa: E402
 from harpy import shape as sh  # noqa: E402
 from harpy import table as tb  # noqa: E402
-from harpy import utils
 
 __all__ = [
     "datasets",
