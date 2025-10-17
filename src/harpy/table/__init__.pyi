efec27255428305352d86309f1d78bd8fe6cb6d1
julienmortier <julien.mortier@hotmail.com>
2025-10-17 15:32:03 +0200

    added nhood_clustering

--- src/harpy/table/__init__.pyi
+++ src/harpy/table/__init__.pyi
@@ -3,6 +3,7 @@ from ._allocation_intensity import allocate_intensity
 from ._annotation import cluster_cleanliness, score_genes, score_genes_iter
 from ._clustering import kmeans, leiden
 from ._enrichment import nhood_enrichment
+from ._nhood_clustering import nhood_count, nhood_kmeans
 from ._preprocess import preprocess_proteomics, preprocess_transcriptomics
 from ._regionprops import add_regionprop_features
 from ._table import add_table_layer, correct_marker_genes, filter_on_size
@@ -24,6 +25,8 @@ __all__ = [
     "kmeans",
     "leiden",
     "nhood_enrichment",
+    "nhood_count",
+    "nhood_kmeans",
     "preprocess_proteomics",
     "preprocess_transcriptomics",
     "add_regionprop_features",
