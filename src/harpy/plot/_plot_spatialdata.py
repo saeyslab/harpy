5dc4adbc8fd872bf90d4995bca4fe4fc0502712f
ArneDefauw <arnedefauw@gmail.com>
2025-10-27 16:47:12 +0100

    plot spatialdata

--- src/harpy/plot/_plot_spatialdata.py
+++ src/harpy/plot/_plot_spatialdata.py
@@ -7,11 +7,21 @@ from types import MappingProxyType
 from typing import Any
 
 import pandas as pd
-import spatialdata_plot  # noqa: F401
 from matplotlib.axes import Axes
 from spatialdata import SpatialData, bounding_box_query
 
 from harpy.utils._keys import _REGION_KEY
+from harpy.utils.pylogger import get_pylogger
+
+log = get_pylogger(__name__)
+
+
+try:
+    import spatialdata_plot  # noqa: F401
+except ImportError:
+    log.warning(
+        "Module 'spatialdata-plot' not installed, please install 'spatialdata-plot' if you want to use 'hp.pl.plot_spatialdata'."
+    )
 
 
 def plot_spatialdata(
@@ -23,10 +33,12 @@ def plot_spatialdata(
     ax: Axes | None = None,
     crd: tuple[int, int, int, int] | None = None,
     to_coordinate_system: str = "global",
+    region_key: str = _REGION_KEY,
     render_images_kwargs: Mapping[str, Any] = MappingProxyType({}),
     render_labels_kwargs: Mapping[str, Any] = MappingProxyType({}),
     show_kwargs: Mapping[str, Any] = MappingProxyType({}),
 ) -> Axes:
+    # TODO write docstring. Write unit tests.
     if table_layer is not None and labels_layer is None:
         raise ValueError(
             f"Please specify a labels layer (which annotates the table layer '{table_layer}') if 'table_layer' is specified."
@@ -35,7 +47,7 @@ def plot_spatialdata(
     if table_layer is not None:
         adata = sdata[table_layer]
         # sanity check
-        mask = adata.obs[_REGION_KEY] == labels_layer
+        mask = adata.obs[region_key] == labels_layer
         if not mask.any():
             raise ValueError(
                 f"The labels layer '{labels_layer}' does not seem to annotate the table layer '{table_layer}."
@@ -75,8 +87,10 @@ def plot_spatialdata(
         if "color" in render_labels_kwargs.keys():
             if queried:
                 color = render_labels_kwargs["color"]
-                if color in sdata_to_plot[table_layer].obs and pd.api.types.is_categorical_dtype(
-                    sdata_to_plot[table_layer].obs[color]
+                if (
+                    color is not None
+                    and color in sdata_to_plot[table_layer].obs
+                    and pd.api.types.is_categorical_dtype(sdata_to_plot[table_layer].obs[color])
                 ):
                     sdata_to_plot[table_layer].obs[color] = (
                         sdata_to_plot[table_layer].obs[color].cat.remove_unused_categories()
