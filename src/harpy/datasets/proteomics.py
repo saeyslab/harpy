6dbbc44329fe1bce1aeb466b0eb1e31a3aa25d9d
ArneDefauw <arnedefauw@gmail.com>
2026-05-24 20:06:03 +0200

    vectra dataset

--- src/harpy/datasets/proteomics.py
+++ src/harpy/datasets/proteomics.py
@@ -3,12 +3,13 @@ from __future__ import annotations
 import os
 from pathlib import Path
 
+import dask.array as da
 import pooch
 import spatialdata as sd
 import tifffile
 from numpy.typing import NDArray
 from spatialdata import SpatialData, read_zarr
-from spatialdata.transformations import get_transformation
+from spatialdata.transformations import Identity, Scale, get_transformation
 
 from harpy.datasets.registry import get_ome_registry, get_registry, get_spatialdata_registry
 from harpy.image._image import add_image
@@ -205,16 +206,47 @@ def imc_example():
     return sdata
 
 
-def vectra_example():
-    """Example proteomics dataset LuCa-7color_[13860,52919]_1x1 from Perkin Elmer"""
-    # Fetch and unzip the file
-    registry = get_ome_registry()
+def vectra_example(path: str | Path | None = None) -> SpatialData:
+    """Example proteomics dataset LuCa-7color_[13860,52919]_1x1 from Perkin Elmer
+
+    Parameters
+    ----------
+    path
+        If ``None``, the example data will be downloaded into the default cache
+        directory for your OS. Provide a custom path to change this behavior.
+
+    """
+    # Fetch and read the TIFF file
+    registry = get_ome_registry(path)
     path = registry.fetch("Vectra-QPTIFF/perkinelmer/PKI_fields/LuCa-7color_%5b13860,52919%5d_1x1component_data.tif")
     input_data, physical_pixel_size_x, physical_pixel_size_y = read_tifffile(path)
     assert physical_pixel_size_x == physical_pixel_size_y
-    # TODO use pixel metadata to set the pixel size
-    sdata = sd.SpatialData(images={"image": sd.models.Image2DModel.parse(input_data, dims="cyx")})
+    pixels_to_microns = Scale(axes=("x", "y"), scale=[physical_pixel_size_x, physical_pixel_size_y])
+    channels = [
+        "PDL1",
+        "CD8",
+        "FoxP3",
+        "CD68",
+        "PD1",
+        "CK",
+        "DAPI",
+        "Autofluorescence",
+    ]
+
+    sdata = sd.SpatialData()
+
+    sdata = add_image(
+        sdata,
+        arr=da.from_array(input_data, chunks=(1, 1024, 1024)),
+        output_image_name="image",
+        c_coords=channels,
+        scale_factors=[2, 2, 2],
+        transformations={"global": Identity(), "global_micron": pixels_to_microns},
+        overwrite=True,
+    )
+
     sdata.path = None
+
     return sdata
 
 
