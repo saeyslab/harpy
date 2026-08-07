f7082c7480d76d72be34e1f510748dea55ae1dcb
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-08-07 11:00:32 +0200

    Run pre-commit hooks

--- src/harpy/image/segmentation/segmentation_models/_cellpose.py
+++ src/harpy/image/segmentation/segmentation_models/_cellpose.py
@@ -71,7 +71,7 @@ def cellpose_callable(
         See documentation of `cellpose.models.CellposeModel.eval` for full description.
     invert
         Invert image pixel intensity before running network. Defaults to `False`.
-        Only supported for cellpose versions `<4.2.0` and raises a `ValueError` on newer versions. 
+        Only supported for cellpose versions `<4.2.0` and raises a `ValueError` on newer versions.
     diameter
         The estimated diameter of cells (in pixels).
     flow_threshold
@@ -196,10 +196,10 @@ def cellpose_callable(
     if cellpose_version < version.parse("4.2.0"):
         common_args["invert"] = invert
     elif invert:
-      raise ValueError(
-          "`invert` was removed from 'CellposeModel.eval' in cellpose>=4.2.0. "
-          "Please invert the image before passing with it with `invert=False` to `harpy.im.segment`, or pin 'cellpose<4.2.0'."
-      )
+        raise ValueError(
+            "`invert` was removed from 'CellposeModel.eval' in cellpose>=4.2.0. "
+            "Please invert the image before passing with it with `invert=False` to `harpy.im.segment`, or pin 'cellpose<4.2.0'."
+        )
     results = model.eval(**common_args)
 
     masks = results[0][0]
